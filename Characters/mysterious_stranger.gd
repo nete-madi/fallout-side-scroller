81512f71c19f9aa8a39b5df02002fe2911ae1ebd
whatsupdawg <netemadi20@gmail.com>
2024-08-23 21:09:37 -0700

    initial commit and setup

--- Characters/mysterious_stranger.gd
+++ Characters/mysterious_stranger.gd
@@ -1,8 +1,6 @@
 extends CharacterBody2D
 
-enum char_state {idle, jump, run}
-
-@export var move_speed: float = 100
+@export var move_speed: float = 200
 @export var jump_height: float = 50
 @export var jump_to_peak: float = 50
 @export var jump_to_descent: float = 50
@@ -16,53 +14,38 @@ enum char_state {idle, jump, run}
 @onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_to_peak * jump_to_peak)) * -1.0
 @onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_to_descent * jump_to_descent)) * -1.0
 
-var move_direction : Vector2 = Vector2.ZERO
+const JUMP_VELOCITY = -400.0
+
 var current_sprite : Sprite2D = idle
 
-
-func _ready():
-	select_new_direction()
-	
-func select_new_direction():
-	# we want our character to begin moving right
-	move_direction = Vector2(1, 1)
-
-func get_gravity_override() -> float:
-	return jump_gravity if velocity.y < 0.0 else fall_gravity
-
-func _physics_process(delta):
-	# Get input direction. Right is a positive value, and left is a negative value
-	var input_direction = Vector2(
+func _physics_process(delta: float) -> void:
+	var run_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"), 0
 	)
-	var jump_direction = Vector2(0, Input.get_action_strength("jump"))
-		
-	if (input_direction.x < 0):
+	if (run_direction.x < 0):
 		current_sprite.flip_h = true
-	elif (input_direction.x > 0):
+	elif (run_direction.x > 0):
 		current_sprite.flip_h = false
-	elif (jump_direction.y > 0):
-		velocity.y = get_gravity_override() * delta
-		char_jump(jump_direction)
-	else:
-		update_animation_params(input_direction)
-	
+	update_animation_params(run_direction)
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
 	# update velocity
-	velocity.x = input_direction.x * move_speed
+	velocity.x = run_direction.x * move_speed
 	move_and_slide()
 	pick_new_state()
 
-func char_jump(jump_direction):
-	velocity.y = jump_velocity
-	update_animation_params(jump_direction)
-	
 func update_animation_params(move_input: Vector2):
 	# Don't change params if there is no move input
+	print(move_input)
 	if (move_input.x != 0):
 		animation_tree.set("parameters/Run/blend_position", move_input)
 		animation_tree.set("parameters/Idle/blend_position", move_input)
-	elif(move_input.y > 0):
-		animation_tree.set("parameters/Jump/blend_position", move_input)
 
 # Choose state based on what is happening with the player
 func pick_new_state():
@@ -78,8 +61,14 @@ func pick_new_state():
 		idle.show()
 		run.hide()
 		jump.hide()
-	elif (velocity.y < 0):
+	if (velocity.y < 0):
 		state_machine.travel("Jump")
+		current_sprite = jump
+		idle.hide()
+		run.hide()
+		jump.show()
+	if (velocity.y > 0):
+		state_machine.travel("Fall")
 		current_sprite = jump
 		idle.hide()
 		run.hide()
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fallout-side-scroller
 An apocalyptic wasteland-themed side scroller, made with Godot 4.3.
+
+## Changelog:
+0.0.1
+- Initial setup. Putting together the first level.
+- Got animations all working!
